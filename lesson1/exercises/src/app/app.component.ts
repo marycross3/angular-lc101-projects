fcaa6593671af11b4b36a0a629c9e80fd94435b8
marycross3 <marycradick@gmail.com>
2023-03-07 19:02:39 -0600

    done

--- lesson1/exercises/src/app/app.component.ts
+++ lesson1/exercises/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-
+  faveLinks = ["https://mail.google.com/mail/u/0/?tab=rm&ogbl", "https://slack.com/"]
 }
