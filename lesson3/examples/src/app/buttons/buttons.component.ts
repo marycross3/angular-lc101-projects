a265c6d7b9512f0f6c4849742b022f0c1701a8f7
marycross3 <marycradick@gmail.com>
2023-03-08 18:52:16 -0600

    done

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -8,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
    inactive: boolean = false;
+   buttonState: boolean[] = [true,true, true];
+   location: string = 'center';
 
    constructor() { }
 
    ngOnInit() { }
 
+   reactivateButtons(){
+      this.buttonState = [true, true, true]
+      // for (let i = 0; i < this.buttons.length; i++){
+      //    this.buttons[i] = true;
+      // }
+      return this.buttonState;
+   }  
+   
+   shiftLocation(oldLocation: string){
+      while (this.location === oldLocation){
+         this.location = ['left', 'right','center'][Math.floor(Math.random()*3)];
+      }
+      return this.location
+   }
 }
+
+
